0279c5efdbdad5f0116530bf103758cad68a6eea
Dag Atle Stenstad <57793052+dagatlestenstad@users.noreply.github.com>
2021-08-02 23:42:30 +0200

    Add support for daily and weekly calendar.

--- app.py
+++ app.py
@@ -11,16 +11,19 @@ confluence = Confluence(
     password=config.confluence_password)
 
 
-def update_confluence_calender_page(confluence_id):
+def update_confluence_calender_page(confluence_id, unit):
 
-    week_number = datetime.date.today().isocalendar()[1]
+    if unit == "w":
+        unit = datetime.date.today().isocalendar()[1]
+    else:
+        unit = datetime.datetime.today().day
 
     confluence_content = confluence.get_page_by_id(confluence_id, expand="body.storage") 
  
     html = confluence_content["body"]["storage"]["value"]
     title = confluence_content["title"]
 
-    column = htmlparser.find_column_number(html, str(week_number))
+    column = htmlparser.find_column_number(html, str(unit))
     row = htmlparser.find_row_number(html, column, "B")
     teddy = htmlparser.get_cell_value(html, 0, row)
 
@@ -33,10 +36,12 @@ def update_confluence_calender_page(confluence_id):
 
 
 if __name__ == '__main__':
-   
-       with open("jobs.yaml", 'r') as stream:
+      
+    with open("jobs.yaml", 'r') as stream:
         data_loaded = yaml.safe_load(stream)
    
         for item in data_loaded['UpdateConfluenceCalender']:
             confluence_id = data_loaded['UpdateConfluenceCalender'][item]['Confluence_id']
-            update_confluence_calender_page(confluence_id)
+            unit = data_loaded['UpdateConfluenceCalender'][item]['Unit']
+
+            update_confluence_calender_page(confluence_id, unit)
